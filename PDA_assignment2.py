9266052b3bfbcf7ad8225e68545aa665340d3101
Kavya Godala <kavyagodala@Kavyas-MacBook-Air.local>
2025-11-07 11:43:18 +0530

    Initial commit for pda_tasks

---
+++ PDA_assignment2.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("Enter any value:"))
+b=int(input("Enter any value:"))
+print(a+b)
+
+
+# In[4]:
+
+
+a=float(input("Enter any value:"))
+b=float(input("Enter any value:"))
+
+
+# In[5]:
+
+
+age=int(input("Enter your age:"))
+years_left=100-age
+print("Years left to reach 100 is:",years_left)
+
+
+# In[6]:
+
+
+firstname=input("Enter your firstname:")
+lastname=input("Enter your lastname:")
+print(firstname,lastname)
+
+
+# In[8]:
+
+
+radius=float(input("Enter radius of a circle:"))
+area_of_circle=3.14*radius*radius
+print(area_of_circle)
+
+
+# In[9]:
+
+
+a=int(input("Enter any value:"))
+b=a*2
+print(b)
+
+
+# In[10]:
+
+
+a=input("Enter any math expression:")
+result=eval(a)
+print(result)
+
+
+# In[11]:
+
+
+a=input("Enter any value:")
+print(a,type(a))
+b=int(a)
+c=b+10
+print(c)
+
+
+# In[12]:
+
+
+a=float(input("Enter any value:"))
+print(a,type(a))
+b=int(a)
+print(b,type(b))
+
+
+# In[13]:
+
+
+user_name=input("Enter user name:")
+print("Welccome",user_name)
+
+
+# In[14]:
+
+
+a=12
+b=5
+print(a+b)
+
+
+# In[18]:
+
+
+x=10
+y=5
+print((x*y)-x,y)
+
+
+# In[16]:
+
+
+x=True
+print(x,type(x))
+
+
+# In[19]:
+
+
+x=17
+y=4
+print(x%y)
+
+
+# In[20]:
+
+
+print("result:",(2*3)-4)
+
+
+# In[21]:
+
+
+a=int(input("Form a number with 5,6,9:"))
+print(a,type(a))
+
+
+# In[22]:
+
+
+a=10
+b=25
+print(a-b)
+
+
+# In[23]:
+
+
+num=99
+print(num+1)
+
+
+# In[24]:
+
+
+num=4**2
+print(num+1)
+
+
+# In[25]:
+
+
+num=6*10
+print(num-5)
+
+
+# In[27]:
+
+
+print("sum:",1.234+2.456)
+
+
+# In[28]:
+
+
+radius=2.5
+area_of_circle=3.14*radius*radius
+print(area_of_circle)
+
+
+# In[29]:
+
+
+a=2.5
+print(a,type(a))
+print(1000*a)
+
+
+# In[30]:
+
+
+a=2.5
+b=4+a
+print(b,type(b))
+
+
+# In[32]:
+
+
+a=2.5
+b=3.5
+c=7.0
+avg=a+b+c/3
+print(avg)
+
+
+# In[33]:
+
+
+a=5
+print(a,type(a))
+b=2.0
+print(b,type(b))
+
+
+# In[34]:
+
+
+a=4
+b=2.0
+c=a*b
+print(c)
+
+
+# In[35]:
+
+
+x=20
+print("Boolean value of x:",bool(x))
+
+
+# In[36]:
+
+
+num=5
+print("Before:",type(num))
+num=num/2
+print("After:",type(num))
+
+
+# In[37]:
+
+
+x=0
+print("Boolean value of x:",bool(x))
+
+
+# In[ ]:
+
+
+
+
